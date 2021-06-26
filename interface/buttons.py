4bffc8d193c5719b042d29e817009b7514b03195
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-06-25 22:26:27 -0300

    Update buttons.py

--- interface/buttons.py
+++ interface/buttons.py
@@ -4,20 +4,20 @@ def main():
 	btn = Button(window, text="View exams", fg='black')
 	btn.place(x=80, y=100)
   
-  btn = Button(window, text="Make a new exam", fg='black')
-  btn.place(x=300, y=160)
-  
-  btn = Button(window, text="Edit exam", fg='black')
-  btn.place(x=300, y=130)
-  
-  btn = Button(window, text="Delete exam", fg='black')
-  btn.place(x=300, y=100)
-  
-  btn = Button(window, text="With 1 themes", fg='black')
-  btn.place(x=450, y=160)
-  
-  btn = Button(window, text="Multiple themes", fg='black')
-  btn.place(x=450, y=190)
+  	btn = Button(window, text="Make a new exam", fg='black')
+  	btn.place(x=300, y=160)
+  	
+  	btn = Button(window, text="Edit exam", fg='black')
+  	btn.place(x=300, y=130)
+  	
+  	btn = Button(window, text="Delete exam", fg='black')
+  	btn.place(x=300, y=100)
+  	
+  	btn = Button(window, text="With 1 themes", fg='black')
+  	btn.place(x=450, y=160)
+  	
+  	btn = Button(window, text="Multiple themes", fg='black')
+  	btn.place(x=450, y=190)
 
 
   
